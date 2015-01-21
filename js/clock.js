634e5c9c092499e0a4cfcb0a3fff9c8478595508
AzureEnigma <dmangin@calpoly.edu>
2015-01-20 17:44:59 -0800

    Clock will update weather
    - Will update based on location
    - BUG: Firefox will never timeout if location sharing is not allowed

--- js/clock.js
+++ js/clock.js
@@ -15,4 +15,95 @@ function getTime()
 	var t = setTimeout(function(){ getTime() }, 1000);
 }
 
-getTime();
+function getLocation() {
+	var coords;
+	if("geolocation" in navigator && navigator.cookieEnabled) {
+		console.log("Geolocator avaliable");
+		console.log(navigator.cookieEnabled);
+		
+		var options = {
+			enableHighAccuracy: true,
+			timeout: 5000,
+			maximumAge: 0
+		}
+		
+		function success(pos) {
+			console.log("got coords");
+			coords = pos.coords;
+			getTemp(coords);
+		}
+		
+		
+		function error(err) {
+			console.warn('ERROR(' + err.code + '): ' + err.message);
+			getTemp(coords);
+		};
+		
+		navigator.geolocation.getCurrentPosition(success, error, success);
+	}
+	else {
+		console.log("Geolocator not avaliable");
+		getTemp(coords);
+	}
+}
+
+function getCityName(latitude, longitude) {
+	var geocoder;
+	geocoder = new google.maps.Geocoder();
+	var latlng = new google.maps.LatLng(latitude, longitude);
+	
+	function success(results) {
+		console.log(results);
+		$("#cityname").html(results[1]["formatted_address"]);
+	}
+	
+	geocoder.geocode({'latLng':latlng}, success);
+}
+
+function determineTemperature(amount) {
+	if(amount >= 90) {
+		return "hot";
+	}
+	else if(amount >= 80) {
+		return "warm";
+	}
+	else if(amount >= 70) {
+		return "nice";
+	}
+	else if(amount >= 60) {
+		return "chilly";
+	}
+	else if(amount < 60) {
+		return "cold";
+	}
+}
+
+function getTemp(coords) {
+	var url;
+	if(coords) {
+		url = "https://api.forecast.io/forecast/bd4fe38b204a091c1570668611842663/" + coords.latitude + "," + coords.longitude + "?callback=?";
+		console.log("coords in URL");
+		getCityName(coords.latitude, coords.longitude);
+	}
+	else {
+		url = "https://api.forecast.io/forecast/bd4fe38b204a091c1570668611842663/35.300399,-120.662362?callback=?";
+		console.log("got nothing");
+		getCityName(35.300399, -120.662362);
+	}
+	var success = function(response) {
+		$("#forecastLabel").html(response["daily"]["summary"]);
+		var icon = "img/" + response["daily"]["icon"] + ".png";
+		$("#forecastIcon").attr("src", icon);
+		var temperature = determineTemperature(response["daily"]["data"][0]["temperatureMax"]);
+		$("body").addClass(temperature);
+	}
+	
+	$.getJSON(url,success);
+}
+
+function onStart() {
+	getTime();
+	getLocation();
+}
+
+onStart();
