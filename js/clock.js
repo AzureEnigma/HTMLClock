6abc79dd981002502e9ed0b232dcf07ef34b0dac
AzureEnigma <dmangin@calpoly.edu>
2015-02-19 11:09:57 -0800

    Added google+ button

--- js/clock.js
+++ js/clock.js
@@ -1,3 +1,27 @@
+var userName = "";
+
+function signinCallback(authResult) {
+  console.log("clicked");
+  if (authResult['status']['signed_in']) {
+    // Update the app to reflect a signed in user
+    // Hide the sign-in button now that the user is authorized, for example:
+    document.getElementById('signinButton').setAttribute('style', 'display: none');
+	console.log(authResult);
+  } else {
+    // Update the app to reflect a signed out user
+    // Possible error values:
+    //   "user_signed_out" - User is signed-out
+    //   "access_denied" - User denied access to your app
+    //   "immediate_failed" - Could not automatically log in the user
+    console.log('Sign-in state: ' + authResult['error']);
+  }
+}
+
+
+function mySignInCallback(userId) {
+
+}
+
 function getAllAlarms()
 {
 	console.log("start");
